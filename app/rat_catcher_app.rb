e0b57be31bed2868d3afb4f0582f1158c140087f
John <jmax@toad.net>
2009-06-03 20:22:39 -0400

    Ripped Gtk references out of app. Renamed rename_method method to replace_node.

--- app/rat_catcher_app.rb
+++ app/rat_catcher_app.rb
@@ -1,110 +1,19 @@
-require 'gtk2'
 require 'app/rat_catcher_store.rb'
-require 'app/rat_catcher_store_gtk_wrapper.rb'
+
 
 class RatCatcherApp
 
-  attr_accessor :main_window,
-                :context_menu,
-                :current_node,
-                :tree_view,
-                :data_file_name,
-                :save_button,
+  attr_accessor :data_file_name,
                 :store
 
   def initialize
-    @tree_view= initialize_tree_view
-    @context_menu= initialize_context_menu
-    @button_bar= initialize_button_bar
-    @main_window= initialize_main_window
-    connect_signals
+    @store= nil
   end
 
-  def initialize_button_bar
-    button_box= Gtk::HButtonBox.new
-
-    @save_button= Gtk::Button.new("Save")
-    button_box.pack_start(@save_button)
-    button_box.show_all
-    button_box.show
-    button_box
-  end
-
-
-  def connect_signals
-    connect_popup_signal(@tree_view)
-    connect_edit_signal(@tree_view)
-    connect_clicked_signal(@save_button, method(:save))
-  end
-
-  def connect_clicked_signal(button, m)
-    button.signal_connect("clicked") do |widget|
-      m.call
-    end
-  end
-
-
-  def initialize_main_window
-    new_main_window= Gtk::Window.new
-    top_box= Gtk::VBox.new
-    new_main_window.add(top_box)
-    top_box.show
-
-    top_box.pack_start(@button_bar)
-    top_box.pack_start(@tree_view)
-
-    new_main_window.show
-    new_main_window
-  end
-
-
-  def initialize_tree_view
-    new_tree_view= Gtk::TreeView.new
-    renderer= Gtk::CellRendererText.new
-    renderer.editable= true
-    new_tree_view.append_column(Gtk::TreeViewColumn.new("",
-                                                        renderer,
-                                                        :text => 0))
-#    @store= RatCatcherStore.new
-#    new_tree_view.model= RatCatcherStoreGtkWrapper.new(@store)
-
-    new_tree_view
-  end
-
-  def connect_popup_signal(a_tree_view)
-    a_tree_view.signal_connect("button_press_event") do |widget, event|
-      popup_context_menu(widget, event)
-    end
-  end
-
-  def connect_edit_signal(a_tree_view)
-    a_tree_view.columns[0].cell_renderers[0].signal_connect("edited") do |renderer, path, new_text|
-      rename_method(renderer,
-                    path[2..-1],
-                    new_text)
-    end
-  end
-
-  def rename_method(renderer, path, new_text)
+  def replace_node(path, new_text)
     node= store.path_reference(path)
     new_sexp= s(:call, node.sexp[1], new_text.to_sym, node.sexp[3])
     node.sexp= new_sexp
-  end
-
-  def initialize_context_menu
-    new_context_menu= Gtk::Menu.new
-    
-    rename_method_item= Gtk::MenuItem.new("Rename Method")
-    rename_method_item.signal_connect("activate") do |w|
-      rename_method_menu_callback
-    end
-    
-    new_context_menu.append rename_method_item
-    new_context_menu.show # This is here only to make specs pass; there
-                          # appears to be a bug in Ruby::Gnome2 such that
-                          # visible? lies if we just do a show_all. Grrr.
-    new_context_menu.show_all
-    new_context_menu
   end
 
   def load(file_name)
@@ -122,15 +31,6 @@ class RatCatcherApp
   end
 
   def run
-    main_window.signal_connect("destroy") { Gtk.main_quit }
-    Gtk.main
-  end
-
-  def popup_context_menu(widget, event)
-    if event.kind_of? Gdk::EventButton and event.button == 3
-      self.current_node= tree_view.get_path_at_pos(event.x, event.y)
-      context_menu.popup(nil, nil, event.button, event.time)
-    end
   end
 
   def save
@@ -139,7 +39,4 @@ class RatCatcherApp
     end
   end
 
-  def rename_method_menu_callback
-    
-  end
 end
