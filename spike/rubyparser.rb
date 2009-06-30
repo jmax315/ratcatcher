2a4f537657f7f83c51d56262d17ebae97068608a
George Dinwiddie <gdinwiddie@idiacomputing.com>
2009-06-29 20:47:59 -0400

    Deleted redundant output and added blank lines between sections.

--- spike/rubyparser.rb
+++ spike/rubyparser.rb
@@ -5,14 +5,10 @@ require 'ruby2ruby'
 
 toy_source= ARGV[0]
 
-print "toy_source: ", toy_source, "\n"
+print "toy_source: ", toy_source, "\n\n"
 
 toy_tree= RubyParser.new.process toy_source
-print "toy_tree:   ", toy_tree, "\n"
+print "toy_tree:   ", toy_tree, "\n\n"
 
 toy_output= Ruby2Ruby.new.process toy_tree
-print "toy_output: ", toy_output, "\n"
-
-print "toy_source: ", toy_source, "\n"
-print "toy_tree:   ", toy_tree, "\n"
-print "toy_output: ", toy_output, "\n"
+print "toy_output: ", toy_output, "\n\n"
