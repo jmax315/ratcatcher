edfda3ab9e679b4e3019898108ec03044730179e
Gregory Casamento <gcasamento@customink.com>
2009-08-10 20:50:21 -0400

    Refactored lasgn_store, var_ref_store.  Added iasgn_store.  Altered variable_spec to test for renaming of instance variable.

--- app/store_nodes/var_ref_store.rb
+++ app/store_nodes/var_ref_store.rb
@@ -2,6 +2,7 @@ class VarRefStore < RatCatcherStore
   def initialize(new_sexp)
     super(new_sexp)
     @text= new_sexp[1].to_s
+    @children << RatCatcherStore.from_sexp(new_sexp[2])
   end
 
   def rename_variable(old_name, new_name)
@@ -11,7 +12,6 @@ class VarRefStore < RatCatcherStore
   end
 
   def sexp
-    s(:lasgn, @text.to_sym, *sexplist_from_children )
+    s(store_type, @text.to_sym, *sexplist_from_children )
   end
-
 end
