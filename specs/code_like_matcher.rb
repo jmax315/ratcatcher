e6c792f6292316891bf85d3214ea4cd2ec7c1286
John <jmax@toad.net>
2009-11-09 18:39:31 -0500

    Checked in code_like matcher (should've gone in last checkin. Whups).

---
+++ specs/code_like_matcher.rb
@@ -0,0 +1,34 @@
+require 'ruby_parser'
+require 'ruby2ruby'
+
+class CodeLikeMatcher
+  def initialize(expected)
+    @expected = expected
+  end
+
+  def normalize(code)
+    RubyParser.new.process(code)
+  end
+
+  def match_helper(expected, target)
+    normalize(expected) == normalize(target)
+  end
+
+  def matches?(target)
+    @target= target
+    match_helper(@expected, target)
+  end
+
+  def failure_message
+    "expected #{@target.inspect} to be code like #{@expected.inspect}"
+  end
+
+  def negative_failure_message
+    "expected #{@target.inspect} not to be code like #{@expected.inspect}"
+  end
+end
+
+
+def be_code_like(expected)
+  CodeLikeMatcher.new(expected)
+end
