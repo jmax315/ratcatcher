01c93caf9cfd0a8684ee4e363ceca8bf78fa83e1
John <jmax@toad.net>
2009-11-09 20:15:37 -0500

    Added DefsStore class for static methods. Added SelfStore class for references to self.

--- specs/todo_spec.rb
+++ specs/todo_spec.rb
@@ -2,6 +2,8 @@ current_dir= File.expand_path(File.dirname(__FILE__))
 require current_dir + '/../app/rat_catcher_store'
 
 describe "specs we don't want to forget" do
-  it "should stay sane if the RatCatcherStore class loader can't find a file it wants to load"
   it "shouldn't depend on an instance variable name to use should_find_the_right_store"
+  it "convert here-doc source code over to %q (makes emacs happier)"
+  it "handle def String.method and similar"
+  it "handle self. expressions"
 end
