371df40aa4bfa015368f2cddb1b2fcd282cff7d0
George Dinwiddie <gdinwiddie@wildebeest.local>
2010-03-29 21:34:42 -0400

    modified command_interpreter for new Rat Catcher Protocol (line count followed by JSON encoded message) at least, for one-line messages.

--- app/rat_catcher_app.rb
+++ app/rat_catcher_app.rb
@@ -1,8 +1,36 @@
 require 'json'
 
+class Array
+  def to_rcp()
+    RatCatcherApp.to_rcp(self)
+  end
+end
+
+class String
+  def to_rcp()
+    RatCatcherApp.to_rcp(self)
+  end
+end
+
+class NilClass
+  def to_rcp()
+    RatCatcherApp.to_rcp(self)
+  end
+end
+
 class RatCatcherApp
+  def self.to_rcp(call)
+    encoded_call= call.to_json
+    "1\n" + encoded_call
+  end
+
+  def self.from_rcp(wrapped_call)
+    count,payload = wrapped_call.split("\n")
+    unwrapped_call= JSON.parse(payload)
+  end
+
   def invoke(wrapped_call)
-    unwrapped_call= JSON.parse wrapped_call
-    send(*unwrapped_call).to_json
+    unwrapped_call= RatCatcherApp.from_rcp(wrapped_call)
+    send(*unwrapped_call).to_rcp
   end
 end
--- specs/command_interpeter_spec.rb
+++ specs/command_interpeter_spec.rb
@@ -1,5 +1,14 @@
-require 'json'
 require File.expand_path(File.dirname(__FILE__)) + '/../app/rat_catcher_app'
+
+describe "encoding the Rat Catcher Protocol" do
+  it "should include line count followed by JSON encoding" do
+    RatCatcherApp.to_rcp("a_string").should == "1\n\"a_string\""
+  end
+
+  it "should encode multi-line strings" do
+    RatCatcherApp.to_rcp("line 1\nline2").should == "1\n\"line 1\\nline2\""
+  end
+end
 
 describe "invoking a method" do
   before :each do
@@ -8,25 +17,31 @@ describe "invoking a method" do
 
   it "should call the method specified" do
     @rat_catcher.should_receive(:do_something)
-    encoded_call= ['do_something'].to_json
+    encoded_call= ['do_something'].to_rcp
     @rat_catcher.invoke(encoded_call)
   end
 
   it "should call a different method" do
     @rat_catcher.should_receive(:do_something_else)
-    encoded_call= ['do_something_else'].to_json
+    encoded_call= ['do_something_else'].to_rcp
     @rat_catcher.invoke(encoded_call)
   end
 
   it "should pass an argument to the method" do
     @rat_catcher.should_receive(:do_something_else).with("an argument")
-    encoded_call= ['do_something_else', 'an argument'].to_json
+    encoded_call= ['do_something_else', 'an argument'].to_rcp
     @rat_catcher.invoke(encoded_call)
   end
 
   it "should receive a return value" do
     @rat_catcher.should_receive(:do_something_else).and_return("ferd")
-    encoded_call= ['do_something_else'].to_json
-    @rat_catcher.invoke(encoded_call).should == "ferd".to_json
+    encoded_call= ['do_something_else'].to_rcp
+    @rat_catcher.invoke(encoded_call).should == "ferd".to_rcp
+  end
+
+  it "should receive a return value with a newline" do
+    @rat_catcher.should_receive(:the_method).and_return("line 1\nline2\n")
+    encoded_call= ['the_method'].to_rcp
+    @rat_catcher.invoke(encoded_call).should == "line 1\nline2\n".to_rcp
   end
 end
