098948cef0e066756cb8b565f5631ea1871fcce1
John Maxwell <john.maxwell@heatinc.com>
2010-05-17 21:04:54 -0400

    Changed all of the RatCatcherApp class methods to be regular methods.

--- app/rat_catcher_app.rb
+++ app/rat_catcher_app.rb
@@ -6,36 +6,36 @@ class RatCatcherApp
     @output_stream = output_stream
   end
 
-  def self.to_rcp(output_stream, json_string)
+  def to_rcp(json_string)
     if (json_string[-1..-1] != "\n")
       json_string += "\n"
     end
-    output_stream.write("#{json_string.count("\n")}\n")
-    output_stream.write(json_string)
+    @output_stream.write("#{json_string.count("\n")}\n")
+    @output_stream.write(json_string)
   end
 
-  def self.from_json(payload)
-    unwrapped_call= JSON.parse(payload)
+  def from_json(payload)
+    JSON.parse(payload)
   end
 
   #TODO change invoke() to return json instead of rcp
   def invoke(wrapped_call)
-    unwrapped_call= RatCatcherApp.from_json(wrapped_call)
+    unwrapped_call= from_json(wrapped_call)
     send(*unwrapped_call)
   end
 
-  def self.from_rcp(input_stream)
-    line_count= input_stream.readline.to_i
+  def from_rcp
+    line_count= @input_stream.readline.to_i
     payload= ""
     line_count.times do
-      payload += input_stream.readline
+      payload += @input_stream.readline
     end
     payload
   end
 
   def interpret_commands
-    wrapped_call= RatCatcherApp.from_rcp(@input_stream)
+    wrapped_call= from_rcp
     result= invoke(wrapped_call)
-    RatCatcherApp.to_rcp(@output_stream, result)
+    to_rcp(result)
   end
 end
--- specs/command_interpeter_spec.rb
+++ specs/command_interpeter_spec.rb
@@ -6,22 +6,22 @@ describe "encoding the Rat Catcher Protocol" do
   end
 
   it "should include line count followed by JSON encoding" do
-    RatCatcherApp.to_rcp(@output_stream, "a_string\n")
+    RatCatcherApp.new(nil, @output_stream).to_rcp("a_string\n")
     @output_stream.string.should == "1\na_string\n"
   end
 
   it "should encode multi-line strings" do
-    RatCatcherApp.to_rcp(@output_stream, "line 1\nline2\n")
+    RatCatcherApp.new(nil, @output_stream).to_rcp("line 1\nline2\n")
     @output_stream.string.should == "2\nline 1\nline2\n"
   end
 
   it "should append a newline if none at end" do
-    RatCatcherApp.to_rcp(@output_stream, "a_string")
+    RatCatcherApp.new(nil, @output_stream).to_rcp("a_string")
     @output_stream.string.should == "1\na_string\n"
   end
 
   it "should even append a newline to multi-line strings if needed" do
-    RatCatcherApp.to_rcp(@output_stream, "line 1\nline2")
+    RatCatcherApp.new(nil, @output_stream).to_rcp("line 1\nline2")
     @output_stream.string.should == "2\nline 1\nline2\n"
   end
 
@@ -31,19 +31,19 @@ describe "decoding the Rat Catcher Protocol" do
   it "should handle 1 line JSON" do
     expected_string= "[\"do_something_else\",\"an argument\"]\n"
     input_stream= StringIO.new("1\n#{expected_string}")
-    RatCatcherApp.from_rcp(input_stream).should == expected_string
+    RatCatcherApp.new(input_stream, nil).from_rcp.should == expected_string
   end
 
   it "should handle two-line JSON" do
     expected_string= "[\"do_something_else\",\n\"an argument\"]\n"
     input_stream= StringIO.new("2\n#{expected_string}")
-    RatCatcherApp.from_rcp(input_stream).should == expected_string
+    RatCatcherApp.new(input_stream, nil).from_rcp.should == expected_string
   end
 
   it "should handle multi-line JSON" do
     expected_string= "[\n\"do_something_else\",\n\"an argument\"\n]\n"
     input_stream= StringIO.new("4\n#{expected_string}")
-    RatCatcherApp.from_rcp(input_stream).should == expected_string
+    RatCatcherApp.new(input_stream, nil).from_rcp.should == expected_string
   end
 end
 
@@ -93,13 +93,13 @@ describe "handling an input stream" do
   it "should call capture a single-line command" do
     encoded_call= "1\n[\"do_something_else\",\"an argument\"]\n"
     input_stream= StringIO.new(encoded_call)
-    RatCatcherApp.from_rcp(input_stream).should == "[\"do_something_else\",\"an argument\"]\n"
+    RatCatcherApp.new(input_stream, nil).from_rcp.should == "[\"do_something_else\",\"an argument\"]\n"
   end
 
   it "should call capture a multi-line command" do
     encoded_call= "4\n[\n  \"do_something_else\",\n  \"an argument\"\n]\n"
     input_stream= StringIO.new(encoded_call)
-    RatCatcherApp.from_rcp(input_stream).should == "[\n  \"do_something_else\",\n  \"an argument\"\n]\n"
+    RatCatcherApp.new(input_stream, nil).from_rcp.should == "[\n  \"do_something_else\",\n  \"an argument\"\n]\n"
   end
 end
 
